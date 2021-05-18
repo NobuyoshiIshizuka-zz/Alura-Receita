58ee0c6c4632830674ee67b572a1b134ffe8c3af
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2021-05-18 20:54:29 -0300

    Parametro na URL

--- receitas/views.py
+++ receitas/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import  get_list_or_404, get_object_or_404, render
 from . models import Receita
 
 
 # Create your views here.
 def index(request):
 
-    receitas = Receita.objects.all() # Receita me devolva todos objetos
+    receitas = Receita.objects.all()  # Receita me devolva todos objetos
 
     dados = {
-       'receitas' : receitas
+        'receitas': receitas
     }
 
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita': receita
+    }
+
+    return render(request, 'receita.html', receita_a_exibir)
