a22b941e47d1087bc2c0c614ad9210990b917fdf
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2021-05-18 20:31:52 -0300

    Exibindo dados do banco

--- receitas/views.py
+++ receitas/views.py
@@ -1,19 +1,14 @@
-
 from django.shortcuts import render
+from . models import Receita
 
 
 # Create your views here.
 def index(request):
 
-    receitas = {
-        1:'Lasanha',
-        2:'Sopa de Legumes',
-        3:'Sorvete',
-        4:'Bolo de chocolate'
-    }
+    receitas = Receita.objects.all() # Receita me devolva todos objetos
 
     dados = {
-       'nome_das_receitas' : receitas
+       'receitas' : receitas
     }
 
     return render(request, 'index.html', dados)
