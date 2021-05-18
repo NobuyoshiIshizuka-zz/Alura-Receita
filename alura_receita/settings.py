b2de32849301ec4553e35c29803509e96c3864e0
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2021-05-18 19:26:09 -0300

    carregando os dados no postgre v1

--- alura_receita/settings.py
+++ alura_receita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alura_receita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'nobuyoshi',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
