f7492e2b8db0fbcb226ec42b42c0560be2a89416
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2021-05-19 09:45:51 -0300

    Exibindo fotos

--- alura_receita/settings.py
+++ alura_receita/settings.py
@@ -129,6 +129,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alura_receita/static')
 ]
 
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
