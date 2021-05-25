f4e20fe2f8cd07723f4151d2973f93cfc4ff2f7e
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2021-05-24 22:34:11 -0300

    adding pagination in the template

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -1,16 +1,21 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
-from . models import Receita
+from receitas.models import Receita
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def index(request):
 
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
+    paginator = Paginator(receitas, 6)
+    page = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(page)
+
 
     dados = {
-        'receitas': receitas
+        'receitas': receitas_por_pagina
     }
 
     return render(request, 'receitas/index.html', dados)
@@ -24,24 +29,6 @@ def receita(request, receita_id):
     }
 
     return render(request, 'receitas/receita.html', receita_a_exibir)
-
-
-def buscar(request):
-
-    lista_receitas = Receita.objects.order_by(
-        '-date_receita').filter(publicada=True)
-
-    if 'buscar' in request.GET:
-        nome_a_buscar = request.GET['buscar']
-        if buscar:
-            lista_receitas = lista_receitas.filter(
-                nome_receita__icontains=nome_a_buscar)
-
-    dados = {
-        'receitas': lista_receitas
-    }
-
-    return render(request, 'receitas/buscar.html', dados)
 
 
 def cria_receita(request):
